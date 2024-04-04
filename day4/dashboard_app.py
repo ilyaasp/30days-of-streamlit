41e4b247bcdc65c3c10acb2702be655c46ceca0d
Ilyas Perlindungan <ipman@Ilyass-MacBook-Air.local>
2024-04-04 15:28:54 +0700

    commit

---
+++ day4/dashboard_app.py
@@ -0,0 +1,22 @@
+# import libs
+import streamlit as st 
+import pandas as pd 
+import numpy as np
+import plotly.graph_objects as go 
+import plotly.express as px
+from datetime import datetime
+
+# define functions
+
+# load data
+
+# engineer data
+## what metrics will be relevant?
+## Difference from baseline?
+## Percent change by video?
+
+# build dashboard
+## total picture?
+## individual video?
+
+# improvements
